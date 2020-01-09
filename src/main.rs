4d1cd155e43f3e3f6c3f9a02c89ebb5278137e0a
Urho Laukkarinen <urho.laukkarinen@gmail.com>
2020-01-09 17:31:30 +0200

    refactored to reduce boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,43 @@
 use std::env;
+use std::collections::HashMap;
 
-fn dna() {
-    println!("[dna] solution: ");
+macro_rules! solutions(
+    { $($key:expr => $value:expr),+ } => {
+        {
+            let mut m : HashMap<&str, &dyn Fn() -> String> = HashMap::new();
+            $(
+                m.insert($key, $value);
+            )+
+            m
+        }
+     };
+);
 
-    let dataset = include_str!("rosalind_dna.txt");
+fn dna() -> String {
+    let dataset = include_str!("rosalind_dna.txt").trim();
 
-    println!("{} {} {} {}",
+    format!("{} {} {} {}",
         dataset.matches('A').count(),
         dataset.matches('C').count(),
         dataset.matches('G').count(),
-        dataset.matches('T').count());
+        dataset.matches('T').count())
 }
 
-fn rna() {
-    println!("[rna] solution: ");
-
-    let dataset = include_str!("rosalind_rna.txt");
-
-    println!("{}", dataset.replace("T", "U"));
+fn rna() -> String {
+    include_str!("rosalind_rna.txt").trim().replace("T", "U")
 }
 
-fn revc() {
-    println!("[revc] solution: ");
-
-    let dataset = include_str!("rosalind_revc.txt");
-
-    let result : String = dataset.chars().map(|c| match c {
+fn revc() -> String {
+    include_str!("rosalind_revc.txt").trim().chars().map(|c| match c {
         'A' => 'T',
         'C' => 'G',
         'G' => 'C',
         'T' => 'A',
         _ => c
-    }).rev().collect();
-
-    println!("{}", result);
+    }).rev().collect::<String>()
 }
 
-fn fib() {
-    println!("[fib] solution: ");
-
+fn fib() -> String {
     let vals : Vec<u64> = include_str!("rosalind_fib.txt")
         .trim().split_whitespace()
         .map(|x|x.parse().unwrap()).collect();
@@ -53,21 +52,26 @@ fn fib() {
         kits = born;
     }
 
-    println!("{}", adults + kits);
+    format!("{}", adults + kits)
 }
 
 fn main() {
+    let solutions = solutions!{
+        "dna" => &dna,
+        "rna" => &rna,
+        "revc"=> &revc,
+        "fib" => &fib
+    };
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
         for i in 1..args.len() {
-            match &args[i][..] {
-                "dna" => dna(),
-                "rna" => rna(),
-                "revc" => revc(),
-                "fib" => fib(),
-                _ => ()
-            };
+            let name = &args[i][..];
+            if let Some(solution) = solutions.get(name) {
+                println!("[{}] solution:", name);
+                println!("{}\n", solution());
+            }
         }
     }
 }
